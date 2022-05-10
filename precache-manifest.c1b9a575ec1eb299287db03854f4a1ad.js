37d7845634d791084d1bc8b26f64517accab72d5
Felipe Segovia <felipe.vsegovia@gmail.com>
2022-05-10 19:15:51 -0300

    Updates

---
+++ precache-manifest.c1b9a575ec1eb299287db03854f4a1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268cc4f213d95751a6546f7a4ce45557",
+    "url": "/project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "f6c003bf85beac73afd5",
+    "url": "/project-movie-cards-library/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "8871f221589d968e98ec",
+    "url": "/project-movie-cards-library/static/js/2.d3c6d777.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-movie-cards-library/static/js/2.d3c6d777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c003bf85beac73afd5",
+    "url": "/project-movie-cards-library/static/js/main.b3fb1e69.chunk.js"
+  },
+  {
+    "revision": "ef07cae08b2844726b46",
+    "url": "/project-movie-cards-library/static/js/runtime-main.2be3e51c.js"
+  }
+]);
